fa13de0021c62cfce1773fc8a41e5e601911ce46
Julia Meireles <juliameirelespessoal@gmail.com>
2024-05-25 02:43:07 -0300

    add: adicionando comentarios

--- Day1/src/Main.java
+++ Day1/src/Main.java
@@ -2,13 +2,11 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args){
-        // creating an instance
-        // i can create instances anywhere in my java app
-        Random generating_random = new Random();
-        int range = 20;
-        int Random_num_in_specific_range = generating_random.nextInt(range + 1);
+        Random generating_random = new Random(); // creating Random object
+        int range = 20; // defining a range 
+        int Random_num_in_specific_range = generating_random.nextInt(range + 1); // using the nextInt(int inbound) method of Random class and storiging it in the variable
 
-       System.out.println("That's a random number within "+range+": "+Random_num_in_specific_range);
+       System.out.println("That's a random number within "+range+": "+Random_num_in_specific_range); // printing the generated number and specifying the range
 
 
     }
