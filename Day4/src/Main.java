320ec1b0ebeb1b3f19479a70b175e674fe419aa1
Julia Meireles <juliameirelespessoal@gmail.com>
2024-05-30 17:57:56 -0300

    feat: :white_check_mark: First method of removing itens: removing itens by index  is working

--- Day4/src/Main.java
+++ Day4/src/Main.java
@@ -1,11 +1,12 @@
 // IMPORT REQUIRED CLASSES
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Iterator;
+//mport java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
       List<String> Cities = new ArrayList<>();
+      RemoveItens remove = new RemoveItens();
 
       Cities.add("Brasilia");
       Cities.add("Florianópolis");
@@ -13,7 +14,24 @@ public class Main {
       Cities.add("Belo Horizonte");
       Cities.add("Rio de Janeiro");
 
+      // removing the second one with remove method
+       remove.removeByIndex(Cities, 1);
+       System.out.println("Removing Florianópolis: "+Cities);
 
+       // removing the fifth one with remove method
+       remove.removeByIndex(Cities, 3);
+       System.out.println("Removing Rio de Janeiro: "+Cities);
+
+        // removing the third one remove method
+        remove.removeByIndex(Cities, 1);
+        System.out.println("Removing São Paulo: "+Cities);
+
+      // removing the forth one remove method
+       remove.removeByIndex(Cities, 1);
+       System.out.println("Removing Belo Horizonte: "+Cities);
+
+
+       
     }
 
 }
--- Day4/src/RemoveItens.java
+++ Day4/src/RemoveItens.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class RemoveItens{
 
     public void removeByIndex(List<String> listaPassada, int index){
-      if (index >=0 && index <= listaPassada.size()){
+      if (index >=0 && index < listaPassada.size()){ // it ensures that index is within the valid range of indices for the list. This valid range is from 0 to lidt.size() - 1
         listaPassada.remove(index);
       }else{
         System.out.println("Index out of bounds");
