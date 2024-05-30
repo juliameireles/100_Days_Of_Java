641bebfbbb65a6166a852f7ee04197f855e0d9b7
Julia Meireles <juliameirelespessoal@gmail.com>
2024-05-30 16:56:31 -0300

    feat: :art: adds required classes

---
+++ Day4/src/RemoveItems.java
@@ -0,0 +1,10 @@
+// IMPORT REQUIRED CLASSES
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+public class RemoveItems {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
